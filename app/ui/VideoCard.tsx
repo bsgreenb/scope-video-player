7a6ff39199b43890f9b0133e0d48dc16752f4d52
Ben Greenberg <bsgreenb@gmail.com>
2024-05-22 21:40:02 -0400

    aspect ratio for video cards

--- app/ui/VideoCard.tsx
+++ app/ui/VideoCard.tsx
@@ -17,30 +17,32 @@ type VideoCardProps = {
 
 const VideoCard = ({id, title, author, thumbnailUrl, lengthSeconds, createdAt, numComments}: VideoCardProps) => {
     return (
-        <div className="bg-white rounded-lg">
-            <div className="relative h-48">
-                <Link href={`/videos/${id}`}>
-                    <Image
-                        src={thumbnailUrl}
-                        alt={title}
-                        layout="fill"
-                        objectFit="cover"
-                        className="rounded-t-lg"
-                    />
-                </Link>
-                <span className="absolute bottom-2 right-2 bg-black text-white text-xs px-2 py-1 rounded">
+        <div className="bg-white">
+            <div className="relative">
+                <div className="aspect-w-16 aspect-h-9">
+                    <Link href={`/videos/${id}`}>
+                        <Image
+                            src={thumbnailUrl}
+                            alt={title}
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded-lg"
+                        />
+                    </Link>
+                </div>
+                <span className="absolute bottom-2 right-2 bg-black text-white text-xs px-2 py-1 rounded z-10">
                     {formatLength(lengthSeconds)}
                 </span>
             </div>
-            <div className="p-4">
+            <div className="py-4">
                 <h3 className="font-bold text-lg">
                     <Link href={`/videos/${id}`}>
                         {title}
                     </Link>
                 </h3>
                 <div className="text-gray-600 text-sm">
-                    <p className="mt-2">
-                        {author} • {formatDistanceToNow(new Date(createdAt))}
+                    <p className="mt-1">
+                        {author} • {formatDistanceToNow(new Date(createdAt))} ago
                     </p>
                     <p className="mt-2">
                         <span className="inline-block mr-1">💬</span>{numComments} comments
