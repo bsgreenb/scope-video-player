5ac504feb54f0b464443a96f7a8f9ebc984c81d2
Ben Greenberg <bsgreenb@gmail.com>
2024-05-24 12:54:32 -0400

    Form with required input for comment

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 type ButtonProps = {
     children: React.ReactNode;
+    type?: "button" | "submit";
 }
 
-const Button = ({children}: ButtonProps) => {
+const Button = ({children, type="button"}: ButtonProps) => {
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button type={type} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             {children}
         </button>
     )
--- app/ui/CommentForm.tsx
+++ app/ui/CommentForm.tsx
@@ -2,10 +2,12 @@ import Button from "./Button";
 
 const CommentForm = () => {
     return (
-        <div className="flex items-center mb-4">
-            <input type="text" placeholder="Add your comment..." className="flex-grow border border-gray-300 rounded-full py-2 px-4 mr-2" />
-            <Button>Comment</Button>
-        </div>
+        <form>
+            <div className="flex items-center mb-4">
+                <input required type="text" placeholder="Add your comment..." className="flex-grow border border-gray-300 rounded-full py-2 px-4 mr-2" />
+                <Button type="submit">Comment</Button>
+            </div>
+        </form>
     );
 }
 
