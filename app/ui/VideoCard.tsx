d56c2f43aa048c97141f719f2b03b3f58450ed0a
Ben Greenberg <bsgreenb@gmail.com>
2024-05-26 14:04:48 -0400

    Next legacy image codemod

--- app/ui/VideoCard.tsx
+++ app/ui/VideoCard.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import { formatDistanceToNow } from 'date-fns';
 import Link from 'next/link';
 import { VideoProps } from '../lib/definitions';
